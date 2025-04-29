4d4e079459faf3a1b096fbb778f53b037e947aed
andkuzm <kandrei704@gmail.com>
2025-04-29 13:09:46 +0300

    extraction try

--- data_processing/llm_test.py
+++ data_processing/llm_test.py
@@ -148,7 +148,15 @@ class Tester_llm:
             decoded_outputs = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
 
             for j, generated in enumerate(decoded_outputs):
-                generated = self.postprocess_prediction(generated.split("**Output:**")[1])
+                if generated.split("**Output:**")[1]:
+                    generated = self.postprocess_prediction(generated.split("**Output:**")[1])
+                elif generated.split("```")[2]:
+                    generated = self.postprocess_prediction(generated.split("```")[2])
+                elif generated.split("**")[2]:
+                    generated = self.postprocess_prediction(generated.split("**")[2])
+                else:
+                    generated = self.postprocess_prediction(generated)
+
                 print(generated)
 
                 # if self.model_name in {"llama3", "llama4", "gemma"}:
