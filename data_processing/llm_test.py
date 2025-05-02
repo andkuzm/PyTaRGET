6b8321e6a7e56bd547cebb0b346ad9aa4e3e995c
andkuzm <kandrei704@gmail.com>
2025-05-02 23:08:53 +0300

    print

--- data_processing/llm_test.py
+++ data_processing/llm_test.py
@@ -248,6 +248,8 @@ class Tester_llm:
     def compute_scores(self, predictions):
         pred_df = pd.DataFrame(predictions)
         num_preds_per_row = len(pred_df.iloc[0]["preds"][0]) if not pred_df.empty else 1
+        print(f"num_preds_per_row={num_preds_per_row}")
+        print(f"num_preds={len(pred_df.iloc[0]["preds"])}")
         eval_size = len(pred_df.iloc[0]["preds"]) * num_preds_per_row
         em_size = 0
         best_preds = []
